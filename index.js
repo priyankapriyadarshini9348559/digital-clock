c2d6a6fc48f52a5c0e92208e4e7db8e605d1756c
priyankapriyadarshini9348559 <136363540+priyankapriyadarshini9348559@users.noreply.github.com>
2025-09-04 09:35:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const hourEl=document.getElementById("hour")
+const minuteEl=document.getElementById("Minute")
+const secondEl=document.getElementById("Second")
+
+
+function updateclock(){
+    let h=new Date().getHours()
+    let m=new Date().getMinutes()
+    let s=new Date().getSeconds()
+    let AMPM="AM"
+    
+    h=h<10?"0"+h:h;
+    m=m<10?"0"+m:m;
+    s=s<10?"0"+s:s;
+    hourEl.innerText=h;
+    minuteEl .innerText=m;
+    secondEl  .innerText=s;
+    
+    setTimeout(updateclock, 1000);
+}
+updateclock()
+
+
+
+
+
